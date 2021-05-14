836ce8699732dbf72251f1464e4247870ff86c9f
Huptam <moisespayares6@gmail.com>
2021-05-13 22:01:45 -0500

    rx01

--- python-rx.py
+++ python-rx.py
@@ -1,1 +1,15 @@
+#Funcion 'print()', para imprimir.
 print("Hello Wolrd")
+
+#Variables:
+x = 2 #Tipo Entero (int)
+r = "hi" #Tipo Caracter (str)
+z = 5.0 #Tipo Flotante (float)
+
+#Así se hacen los comentarios de una linea.
+"""
+Así se hacen
+los comentarios
+de multiples lineas
+"""
+
